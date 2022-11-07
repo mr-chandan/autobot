53662706d7feafde1181cce6eca59c87c10c384b
Chandan H <106821717+mr-chandan@users.noreply.github.com>
2022-11-07 21:53:18 +0530

    Update Autoinsta.py

--- Autoinsta.py
+++ Autoinsta.py
@@ -2,28 +2,40 @@ import os
 from instagrapi import Client
 from RedDownloader import RedDownloader
 import time
+import random
 
 inst = Client()
-inst.login('id', 'password')
+inst.login('ID', 'PASSWORD')
 
 
 def getmemes():
-    RedDownloader.DownloadVideosBySubreddit(
-        'IndianDankMemes', 6, flair=None, SortBy="hot", output="downloaded", quality=360, destination=None, cachefile='Downloaded.txt')
+    try:
+        post = RedDownloader.DownloadVideosBySubreddit(
+            'nextfuckinglevel', 5, flair=None, SortBy="hot", output="downloaded", quality=1080, destination=None, cachefile='Downloaded.txt')
+        title = post.GetPostTitles()
+    except:
+        print("error in redddit api")
+    return title
 
+
+tags = ["#love #art #beautiful #follow #life #makeup #bestoftheday #amazing #lifestyle #beach #vscocam #tagsforlikes #sun #family",
+        "#igers #sunset #family #girl #repost #summer #instadaily #style #follow #photography #life #makeup #instagram #love #inspiration",
+        "#peace #peaceful #peacefull #peacefulday #satisfying #satisfyingvideos #satisfyingsounds #satisfyingasmr #satisfyingcleaning #satisfyingslimevideo",
+        "#tagsforlikes #photographer #hair #motivation #smile #instalike #science #sciencefiction #sciencememes #technology #technologynews #informationtechnology",
+        "#technologyfacts #technologyisawesome #memes #mamas #timepass #passingtime #passtime #passeggiatatime #passingthetime #follow4like #pleasefollow #tambahfollowers",
+        "#reelsvideo #instareels #reelsindia #reelit #reeltoreel #reellife #instagramreels #spamforfollow #followlike #followforafollow #cashfollowtrain #followbacknow #followersrealindo"]
 
 while True:
     title = getmemes()
-    for i in range(1, 7):
+    for i in range(1, (len(title)+1)):
         try:
             inst.video_upload(
-                "/home/mtgodlon/downloaded/downloaded"+str(i)+".mp4",
-                "credits to IndianDankMemes")
-            os.remove("/home/mtgodlon/downloaded/downloaded"+str(i)+".mp4")
-            os.remove("/home/mtgodlon/downloaded/downloaded"+str(i)+".mp4.jpg")
+                "downloaded/downloaded"+str(i)+".mp4",
+                title[i-1] + "  " + random.choice(tags))
         except:
             print("error")
-        print("sleaping...............in_for")
-        time.sleep(4680)
-    print("sleaping...............in_while")
-    time.sleep(3600)
+        os.remove("downloaded/downloaded"+str(i)+".mp4")
+        os.remove("downloaded/downloaded"+str(i)+".mp4.jpg")
+        print("sleaping............... "+str(i))
+        time.sleep(7200)
+    title.clear()
